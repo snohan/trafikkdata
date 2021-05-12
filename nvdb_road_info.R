f61aa120d81b17cb51494b3c0356fc55f8ec2f16
Snorre Hansen <snorre.hansen@vegvesen.no>
2021-05-12 15:19:35 +0200

    NR directed graph

---
+++ nvdb_road_info.R
@@ -0,0 +1,10 @@
+# Look at raod net info
+
+source("H:/Programmering/R/byindeks/get_from_nvdb_api.R")
+
+
+# Ser på egenskapene til alle veglenker på en gitt veglenkesekvens
+
+en_veglenkesekvens <- hent_veglenkesekvens("1125914")
+
+# TODO: se på historiske lenker
