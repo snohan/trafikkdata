668fcd2f5fa688093091fafaea18f726763b9ee6
Snorre Hansen <snorre.hansen@vegvesen.no>
2021-10-29 12:22:43 +0200

    Common read all moved to top

--- kibana_csv_formatter_cases.R
+++ kibana_csv_formatter_cases.R
@@ -1,16 +1,34 @@
 #
 source("H:/Programmering/R/trafikkdata/kibana_csv_to_xlsx.R")
 
+# Read all files in folder
+folder_in_focus <- "spesialbestillinger/nordland_pd"
+
+data_from_csv <-
+  list.files(path = folder_in_focus, pattern = "*.csv",
+             full.names = TRUE) %>%
+  map_df(~read_csv2(.))
+
+
 # Kibana vbv data ----
 # Read kibana exported csv file
-data_from_csv <- read.csv2("spesialbestillinger/agderpd2.csv") %>%
-  dplyr::filter(valid_event == "true") %>%
+data_from_csv_enriched <- data_from_csv %>%
+  dplyr::filter(valid_event == TRUE) %>%
   dplyr::rename(trp_id = traffic_registration_point_id) %>%
   dplyr::left_join(points_metadata) %>%
   dplyr::select(trp_id, name, road_reference, county_name, municipality_name,
-                timestamp = event_timestamp, time_gap, lane, length, speed, vehicle_class = vehicle_type_raw,
-                valid_length, valid_speed, valid_class = valid_classification,
-                wrong_direction, datalogger_type) %>%
+                timestamp = event_timestamp,
+                time_gap,
+                lane,
+                length,
+                speed,
+                vehicle_class = vehicle_type_raw,
+                valid_length,
+                valid_speed,
+                valid_class = valid_classification,
+                wrong_direction,
+                datalogger_type
+                ) %>%
   dplyr::arrange(trp_id, timestamp) %>%
   add_correct_direction_names() %>%
   # dplyr::mutate(is_lane_odd = purrr::map(lane, ~ length(is_odd(.)) > 0),
@@ -26,18 +44,11 @@ data_from_csv <- read.csv2("spesialbestillinger/agderpd2.csv") %>%
   dplyr::select(-is_lane_odd, -lane_parity_kibana, -metering_direction_changed, -lane) %>%
   dplyr::relocate(direction:lane_number, .before = timestamp)
 
-writexl::write_xlsx(data_from_csv, path = "spesialbestillinger/agderpd2.xlsx")
+writexl::write_xlsx(data_from_csv_enriched,
+                    path = paste0(folder_in_focus, "/trafikkdata.xlsx"))
 
 
 # Kibana aggregated data ----
-# Read all files in folder
-folder_in_focus <- "spesialbestillinger/salopek"
-
-data_from_csv_agg <-
-  list.files(path = folder_in_focus, pattern = "*.csv",
-             full.names = TRUE) %>%
-  map_df(~read_csv2(.))
-
 data_from_csv_agg_meta <- data_from_csv_agg %>%
   dplyr::rename(trp_id = punkt) %>%
   dplyr::left_join(points_metadata) %>%
