1f10e2c4a0914d378db3c5816e3cef0616a292ab
Snorre Hansen <snorre.hansen@vegvesen.no>
2022-11-25 14:06:10 +0100

    congestion

---
+++ congestion_functions.R
@@ -0,0 +1,334 @@
+## Speed and flow ----
+
+visualize_speed_and_flow <-
+  function(
+    critical_values,
+    aggregated_data,
+    trp_info,
+    lane_info
+    ) {
+
+  svv_background_color <- "#F5F5F5"
+
+  aggregated_data |>
+    ggplot(
+      aes(
+        x = mean_speed,
+        y = flow,
+        color = congestion
+      )
+    ) +
+    geom_point(
+      size = 2,
+      alpha = 0.4
+    ) +
+    facet_wrap(
+      vars(lane),
+      labeller = labeller(lane = lane_info)
+    ) +
+    geom_hline(
+      data = critical_values,
+      aes(
+        yintercept = max_flow
+      ),
+      color = "#444f55"
+    ) +
+    geom_vline(
+      data = critical_values,
+      aes(
+        xintercept = critical_speed
+      ),
+      color = "#444f55"
+    ) +
+    theme_light() +
+    scale_color_manual(
+      values =
+        c("Nei" = "#dadada",
+          "Ja" = "#008ec2"),
+      name = "Redusert kapasitet"
+    ) +
+    theme(
+      panel.grid.minor.x = element_blank(),
+      legend.position = "right",
+      axis.title.x = element_text(
+        margin = margin(t = 15, r = 0, b = 0, l = 0)),
+      axis.text.y = element_text(vjust = 0.5),
+      plot.caption =
+        element_text(
+          face = "italic",
+          size = 8,
+          lineheight = 1.5,
+          vjust = 0
+        ),
+      strip.background =
+        element_rect(
+          colour = "#444f55",
+          fill = "#ed9300"
+        ),
+      strip.text =
+        element_text(
+          colour = "#444f55"
+        ),
+      plot.background = element_rect(fill = svv_background_color),
+      panel.background = element_rect(fill = svv_background_color),
+      legend.background = element_rect(fill = svv_background_color),
+      legend.key = element_blank()
+    ) +
+    labs(
+      x = "Gjennomsnittsfart (km/time)",
+      y = "Trafikkstrøm (kjøretøy/time)",
+      caption = "Data: Statens vegvesen, fylkene."
+    ) +
+    ggtitle(
+      "Gjennomsnittsfart og trafikkstrøm i intervaller på 5 min",
+      subtitle = trp_info
+    )
+
+  }
+
+
+## Density and flow ----
+visualize_density_and_flow <-
+  function(
+    critical_values,
+    aggregated_data,
+    trp_info,
+    lane_info
+  ) {
+
+    svv_background_color <- "#F5F5F5"
+
+    aggregated_data |>
+      ggplot(
+        aes(
+          x = density,
+          y = flow,
+          color = congestion
+        )
+      ) +
+      geom_point(
+        size = 2,
+        alpha = 0.4
+      ) +
+      facet_wrap(
+        vars(lane),
+        labeller = labeller(lane = lane_info)
+      ) +
+      geom_hline(
+        data = critical_values,
+        aes(
+          yintercept = max_flow
+        ),
+        color = "#444f55"
+      ) +
+      geom_vline(
+        data = critical_values,
+        aes(
+          xintercept = road_capacity
+        ),
+        color = "#444f55"
+      ) +
+      theme_light() +
+      scale_color_manual(
+        values =
+          c("Nei" = "#dadada",
+            "Ja" = "#008ec2"),
+        name = "Redusert kapasitet"
+      ) +
+      theme(
+        panel.grid.minor.x = element_blank(),
+        legend.position = "right",
+        axis.title.x = element_text(
+          margin = margin(t = 15, r = 0, b = 0, l = 0)),
+        axis.text.y = element_text(vjust = 0.5),
+        plot.caption =
+          element_text(
+            face = "italic",
+            size = 8,
+            lineheight = 1.5,
+            vjust = 0
+          ),
+        strip.background =
+          element_rect(
+            colour = "#444f55",
+            fill = "#ed9300"
+          ),
+        strip.text =
+          element_text(
+            colour = "#444f55"
+          ),
+        plot.background = element_rect(fill = svv_background_color),
+        panel.background = element_rect(fill = svv_background_color),
+        legend.background = element_rect(fill = svv_background_color),
+        legend.key = element_blank()
+      ) +
+      labs(
+        x = "Trafikktetthet (kjøretøy/km)",
+        y = "Trafikkstrøm (kjøretøy/time)",
+        caption = "Data: Statens vegvesen, fylkene."
+      ) +
+      ggtitle(
+        "Trafikktetthet og trafikkstrøm i intervaller på 5 min",
+        subtitle = trp_info
+      )
+
+  }
+
+
+
+## Particular day ----
+visualize_speed_day <-
+  function(
+    critical_values,
+    aggregated_data,
+    trp_info,
+    lane_info,
+    selected_day
+  ) {
+
+    # selected_day e.g. "2021-10-28"
+
+    svv_background_color <- "#F5F5F5"
+
+    aggregated_data |>
+      dplyr::filter(
+        date == selected_day
+      ) |>
+      ggplot(
+        aes(
+          x = timestamp_floored,
+          y = mean_speed,
+          color = congestion,
+          alpha = density
+        )
+      ) +
+      geom_point(
+        size = 2
+      ) +
+      facet_wrap(
+        vars(lane),
+        labeller = labeller(lane = lane_info)
+      ) +
+      geom_hline(
+        data = critical_values,
+        aes(
+          yintercept = critical_speed
+        ),
+        color = "#444f55"
+      ) +
+      theme_light() +
+      scale_color_manual(
+        values =
+          c("Nei" = "#444f55",
+            "Ja" = "#008ec2"),
+        name = "Redusert kapasitet"
+      ) +
+      scale_alpha(
+        name = "Trafikktetthet"
+      ) +
+      theme(
+        panel.grid.minor.x = element_blank(),
+        legend.position = "right",
+        axis.title.x = element_text(
+          margin = margin(t = 15, r = 0, b = 0, l = 0)),
+        axis.text.y = element_text(vjust = 0.5),
+        plot.caption =
+          element_text(
+            face = "italic",
+            size = 8,
+            lineheight = 1.5,
+            vjust = 0
+          ),
+        strip.background =
+          element_rect(
+            colour = "#444f55",
+            fill = "#ed9300"
+          ),
+        strip.text =
+          element_text(
+            colour = "#444f55"
+          ),
+        plot.background = element_rect(fill = svv_background_color),
+        panel.background = element_rect(fill = svv_background_color),
+        legend.background = element_rect(fill = svv_background_color),
+        legend.key = element_blank()
+      ) +
+      labs(
+        x = NULL,
+        y = "Gjennomsnittsfart (km/time)",
+        caption = "Data: Statens vegvesen, fylkene."
+      ) +
+      ggtitle(
+        "Gjennomsnittsfart i intervaller på 5 min",
+        subtitle = paste0(trp_info, ", ", selected_day)
+      )
+
+  }
+
+
+## Congestion ratio ----
+visualize_congestion_ratio <-
+  function(
+    congestion_stats,
+    trp_info,
+    lane_info
+  ) {
+
+    svv_background_color <- "#F5F5F5"
+
+    congestion_stats |>
+      ggplot(
+        aes(
+          x = weekday,
+          y = congestion_percentage,
+          fill = year
+        )
+      ) +
+      geom_col(
+        position = "dodge"
+      ) +
+      scale_fill_grey() +
+      facet_wrap(
+        vars(lane),
+        labeller = labeller(lane = lane_info)
+      ) +
+      theme_light() +
+      theme(
+        panel.grid.minor.x = element_blank(),
+        legend.position = "right",
+        axis.title.x = element_text(
+          margin = margin(t = 15, r = 0, b = 0, l = 0)),
+        axis.text.y = element_text(vjust = 0.5),
+        plot.caption =
+          element_text(
+            face = "italic",
+            size = 8,
+            lineheight = 1.5,
+            vjust = 0
+          ),
+        strip.background =
+          element_rect(
+            colour = "#444f55",
+            fill = "#ed9300"
+          ),
+        strip.text =
+          element_text(
+            colour = "#444f55"
+          ),
+        plot.background = element_rect(fill = svv_background_color),
+        panel.background = element_rect(fill = svv_background_color),
+        legend.background = element_rect(fill = svv_background_color),
+        legend.key = element_blank()
+      ) +
+      labs(
+        x = NULL,
+        y = "Andel av døgn (%)",
+        caption = "Data: Statens vegvesen, fylkene."
+      ) +
+      ggtitle(
+        "Andel tid av døgn med redusert kapasitet",
+        subtitle = trp_info
+      )
+
+  }
+
---
+++ congestion_metric.R
@@ -0,0 +1,395 @@
+# VbV data from Kibana
+
+library(tidyverse)
+library(hms)
+base::Sys.setlocale(locale = "nb.utf8")
+
+source("H:/Programmering/R/byindeks/get_from_trafficdata_api.R")
+source("H:/Programmering/R/byindeks/split_road_system_reference.R")
+# Literature
+# A Survey of Methods and Technologies for Congestion Estimation Based on Multisource Data Fusion
+# https://www.mdpi.com/2076-3417/11/5/2306/htm
+
+# https://research.geodan.nl/measuring-traffic-congestion-a-better-way/
+
+# TRP ----
+trp <-
+  get_points_with_direction() |>
+  dplyr::distinct(trp_id, .keep_all = T)
+
+
+# Read ----
+# some_data <-
+#   readr::read_csv2(
+#     "congestion_data/kanalbrua_2021.csv"
+#     #"congestion_data/sundland_4.csv"
+#   )
+
+# Read Kibana-exported CSVs ----
+read_a_file <- function(file_name) {
+
+  readr::read_csv2(
+    paste0("congestion_data/", file_name)
+  )
+
+}
+
+# Choose which files to read
+filename_root <- "kanalbrua_2021"
+
+# Do read
+vbv_data <-
+  list.files(
+    "congestion_data",
+    pattern = paste0("^", filename_root, ".*csv")
+  ) |>
+  purrr::map_df(
+    ~ read_a_file(.)
+  )
+
+# Smoothing by 5 in intervals
+# Alternative smoothing: exponential average of vbv
+aggregated_data <-
+  vbv_data |>
+  dplyr::rename(
+    trp_id = traffic_registration_point_id
+  ) |>
+  dplyr::filter(
+    valid_event == TRUE
+  ) |>
+  dplyr::mutate(
+    event_timestamp =
+      lubridate::ymd_hms(
+        event_timestamp,
+        tz = "CET"
+        ),
+     timestamp_floored =
+      lubridate::floor_date(
+        event_timestamp,
+        "5 mins"
+      ),
+    interval_start = hms::as_hms(timestamp_floored),
+    date =
+      lubridate::date(event_timestamp),
+    valid_speed =
+      dplyr::if_else(
+        valid_speed == TRUE,
+        1,
+        0,
+        0
+      ),
+    # speed =
+    #   dplyr::if_else(
+    #     valid_speed == TRUE,
+    #     speed,
+    #     NA_real_,
+    #     NA_real_
+    #   )
+  ) |>
+  dplyr::group_by(
+    trp_id,
+    timestamp_floored,
+    date,
+    interval_start,
+    lane
+  ) |>
+  dplyr::summarise(
+    volume = n(),
+    volume_with_valid_speed = sum(valid_speed),
+    mean_speed = mean(speed),
+    mean_time_gap = mean(time_gap),
+    .groups = "drop"
+  ) |>
+  dplyr::mutate(
+    flow = volume / 5 * 60, # Explicitly using 5 min aggregates
+    density = flow / mean_speed
+  )
+
+
+# When is traffic congested?
+# Find maximum flow for a longer period, i.e. month.
+# Find what mean speed is for that max flow.
+# Then congestion occurs in all intervals with less than critical mean speed.
+# To avoid single slow vehicles at night being tagged as congested, the
+# mean time gap should be less than 5 s.
+
+# TODO:
+# Using personal car equivalents?
+
+critical_values <-
+  aggregated_data |>
+  dplyr::group_by(
+    lane
+  ) |>
+  dplyr::slice_max(
+    order_by = flow,
+    #n = 15,
+    prop = 0.005
+  ) |>
+  dplyr::summarise(
+    max_flow = median(flow),
+    critical_speed = median(mean_speed),
+    road_capacity = median(density),
+    .groups = "drop"
+  )
+
+# critical_values_2 <-
+#   aggregated_data |>
+#   dplyr::group_by(
+#     lane
+#   ) |>
+#   dplyr::summarise(
+#     q3_flow = quantile(flow, probs = c(0.75)),
+#     q1_flow = quantile(flow, probs = c(0.25)),
+#     iqr = q3_flow - q1_flow,
+#     max_flow = q3_flow + 1.5 * iqr,
+#     #critical_speed = median(mean_speed),
+#     #road_capacity = median(density),
+#     .groups = "drop"
+#   )
+
+
+data_congested <-
+  aggregated_data |>
+  dplyr::left_join(
+    critical_values,
+    by = "lane"
+  ) |>
+  dplyr::mutate(
+    congestion =
+      dplyr::case_when(
+        mean_speed < critical_speed &
+          #mean_time_gap < 5 &
+          density >= road_capacity ~ "Ja",
+        TRUE ~ "Nei"
+      ),
+    weekday =
+      lubridate::wday(
+        date,
+        label = TRUE,
+        abbr = TRUE,
+        week_start = 1
+      )
+  )
+
+congestion_stats <-
+  data_congested |>
+  dplyr::group_by(
+    trp_id,
+    weekday,
+    lane
+  ) |>
+  dplyr::count(
+    congestion
+  ) |>
+  dplyr::ungroup() |>
+  tidyr::complete(
+    trp_id,
+    weekday,
+    lane,
+    congestion,
+    fill = list(n = 0)
+  ) |>
+  tidyr::pivot_wider(
+    names_from = "congestion",
+    values_from = "n"
+  ) |>
+  dplyr::mutate(
+    congestion_percentage =
+      Ja / (Ja + Nei) * 100
+  )
+
+readr::write_rds(
+  congestion_stats,
+  file =
+    paste0("congestion_data/", filename_root, "_stats.rds")
+)
+
+
+find_trp_info_and_direction_names <- function(aggregated_data) {
+
+  # Needs a "trp" df with all TRP info from Trafikkdata API
+
+  trp_here <-
+    trp |>
+    dplyr::filter(
+      trp_id == data_congested$trp_id[1]
+    ) |>
+    split_road_system_reference()
+
+  trp_info <-
+    base::paste0(
+      trp_here$road_category_and_number,
+      " ",
+      stringr::str_to_title(trp_here$name),
+      ", ",
+      trp_here$municipality_name
+    )
+
+  trp_direction_names <-
+    trp_here |>
+    dplyr::select(
+      from,
+      to
+    ) |>
+    tidyr::pivot_longer(
+      cols = c(from, to),
+      names_to = "trp_direction",
+      values_to = "direction_name_to"
+    ) |>
+    dplyr::mutate(
+      direction_name_to = stringr::str_to_title(direction_name_to)
+    )
+
+  trp_directions <-
+    data_congested |>
+    dplyr::distinct(lane) |>
+    dplyr::mutate(
+      trp_direction = dplyr::if_else(lane %% 2 == 0, "from", "to")
+    ) |>
+    dplyr::left_join(
+      trp_direction_names,
+      by = "trp_direction"
+    ) |>
+    dplyr::mutate(
+      name_string = paste0(
+        "Felt ",
+        lane,
+        ": til ",
+        direction_name_to
+      )
+    )
+
+  lane_names <-
+    trp_directions$name_string
+
+  names(lane_names) <-
+    trp_directions$lane
+
+  result <- list(
+    trp_info,
+    lane_names
+  )
+
+  return(result)
+}
+
+
+
+# Write ----
+find_trp_info_and_direction_names(data_congested) |>
+  readr::write_rds(
+    file = paste0("congestion_data/", filename_root, "_trp.rds")
+  )
+
+list(
+  critical_values,
+  data_congested
+) |>
+readr::write_rds(
+  file = paste0("congestion_data/", filename_root, ".rds")
+)
+
+# Visually verify ----
+
+## Density and flow ----
+data_congested |>
+  ggplot(
+    aes(
+      x = density,
+      y = flow,
+      color = congestion
+    )
+  ) +
+  geom_point() +
+  facet_wrap(
+    vars(lane)
+  ) +
+  geom_hline(
+    data = critical_values,
+    aes(
+      yintercept = max_flow
+    ),
+    color = "red"
+  ) +
+  geom_vline(
+    data = critical_values,
+    aes(
+      xintercept = road_capacity
+    ),
+    color = "red"
+  )
+
+
+## Speed and density ----
+data_congested |>
+  ggplot(
+    aes(
+      x = density,
+      y = mean_speed,
+      color = congestion
+    )
+  ) +
+  geom_point() +
+  facet_wrap(
+    vars(lane)
+  ) +
+  geom_hline(
+    data = critical_values,
+    aes(
+      yintercept = critical_speed
+    ),
+    color = "red"
+  ) +
+  geom_vline(
+    data = critical_values,
+    aes(
+      xintercept = road_capacity
+    ),
+    color = "red"
+  )
+
+
+## Look at a particular day ----
+data_congested |>
+  dplyr::filter(
+    date == "2022-10-28"
+  ) |>
+  ggplot(
+    aes(
+      x = timestamp_floored,
+      y = mean_speed,
+      color = congestion,
+      alpha = density
+    )
+  ) +
+  geom_point() +
+  facet_wrap(
+    vars(lane)
+  ) +
+  geom_hline(
+    data = critical_values,
+    aes(
+      yintercept = critical_speed
+    ),
+    color = "red"
+  )
+
+
+## Timegaps ----
+data_congested |>
+  dplyr::filter(
+    date == "2021-10-29"
+  ) |>
+  ggplot(
+    aes(
+      x = timestamp_floored,
+      y = mean_time_gap
+    )
+  ) +
+  geom_point() +
+  facet_wrap(
+    vars(lane)
+  )
+
+
