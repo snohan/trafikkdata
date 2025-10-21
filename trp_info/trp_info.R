cd613a3e5aa7959a5a934748fce0e366583b5989
Snorre Hansen <snorre.hansen@vegvesen.no>
2025-10-21 14:27:26 +0200

    Updated trp info script to handle mobile trp direction names.

--- trp_info/trp_info.R
+++ trp_info/trp_info.R
@@ -1,11 +1,49 @@
 # TRP metainfo, and map internal lanes to fit current metering
 
-# Internal lane numbers from TRP API
+
+# Internal lane numbers from TRP API ----
 # Made in trp_api_fetcher.R
-trp_info <- readr::read_rds("trp.rds")
+trp_info <- readr::read_rds("trp_info/trp.rds")
 
 
-# Mobile TRPs lack info on lanes
+# Handling mobile TRPs ----
+# Mobile TRPs does not have the concept of internal lane numbers - their lane numbers are per definition according to road link
+trp_info_mobile <-
+  trp_info |>
+  dplyr::filter(
+    trp_type == "MOBILE"
+  ) |>
+  dplyr::mutate(
+    # All mobile TRPs have reference direction given according to current road link
+    direction_with_current_link = TRUE
+  )
+
+# All mobile TRPs have lane numbers 1 and/or 2.
+# Adding both will sometimes be wrong (if they have only one lane),
+# but when joining this to real data, the lanes that doesn't exist will not appear.
+trp_info_mobile_lane <-
+  dplyr::bind_rows(
+    trp_info_mobile |>
+      dplyr::mutate(
+        lane_internal = 1
+      ),
+    trp_info_mobile |>
+      dplyr::mutate(
+        lane_internal = 2
+      )
+  ) |>
+  dplyr::arrange(trp_id) |>
+  dplyr::mutate(
+    lane_according_to_current_metering =
+      dplyr::case_when(
+        same_direction_of_metering_and_link_now ~ lane_internal,
+        !same_direction_of_metering_and_link_now & lane_internal %% 2 == 0 ~ 1,
+        TRUE ~ 2
+      )
+  )
+
+
+# Tables for joining with traffic data ----
 trp_info_no_lane <-
   trp_info |>
   dplyr::select(
@@ -25,7 +63,13 @@ trp_info_lane <-
   )
 
 trp_info_lane_direction_names <-
-  trp_info |>
+  dplyr::bind_rows(
+    trp_info |>
+      dplyr::filter(
+        trp_type != "MOBILE"
+      ),
+    trp_info_mobile_lane
+  ) |>
   dplyr::mutate(
     from =
       dplyr::case_when(
