d259ea5a069c046cdc41ccd629790ba38041ffcc
snohan <snorrh@gmail.com>
2026-02-23 09:36:27 +0100

    Added some description to fartsrapport_prep

--- fartsrapport/fartsrapport_prep.R
+++ fartsrapport/fartsrapport_prep.R
@@ -15,11 +15,18 @@
   }
 }
 
-
-# Given a TRP ID and a folder with its vbv data
-trp_id_chosen <- "97174V1215671"
+# 1. Specify folder which holds the vbv file
+# Use "Discover" in Kibana analysis, must contain
+# trp_id, timestamp, valid_event, valid_speed, speed, lane
 vbv_folder <- "spesialbestillinger/rasen"
+
+# 2. Specify TRP
+trp_id_chosen <- "97174V1215671"
+
+# 3. Look up TRP-location in Vegbilder and paste its URL here
 vegbilde_url <- "https://vegbilder.atlas.vegvesen.no/?year=2025&lat=61.58557224&lng=9.80156209&zoom=16&view=image&imageId=Vegbilder_360_2025.2025-08-21T11.33.30_FV02522_S5D1_m01423_360_2"
 
+# 4. Aggregate data
 source("fartsrapport/fartsrapport_aggreger.R")
 
+# 5. Use fartsrapport.qmd to generate the report in HTML
